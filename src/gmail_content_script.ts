be8b9707c52123f81fb16de9c66e945ab39b8b84
Jeong Seulho <wjdtmfgh@gmail.com>
2024-02-02 19:39:17 +0900

    feat : query file name

--- src/gmail_content_script.ts
+++ src/gmail_content_script.ts
@@ -1,19 +1,27 @@
-const btn = document.createElement('button');
-btn.style.width = '70px';
-btn.style.height = '70px';
-btn.style.position = 'fixed';
-btn.style.bottom = '50px';
-btn.style.right = '0px';
-btn.style.zIndex = '9999';
-btn.style.border = 'none';
-btn.style.borderRadius = '10px';
-btn.style.padding = '0px';
-const imgUrl = chrome.runtime.getURL('/assets/btn.png');
-btn.style.backgroundImage = `url(${imgUrl})`;
-btn.style.backgroundRepeat = 'no-repeat';
-btn.style.backgroundSize = 'contain';
+const createBtn = () => {
+  const btn = document.createElement('button');
+  btn.style.width = '70px';
+  btn.style.height = '70px';
+  btn.style.position = 'fixed';
+  btn.style.bottom = '50px';
+  btn.style.right = '0px';
+  btn.style.zIndex = '9999';
+  btn.style.border = 'none';
+  btn.style.borderRadius = '10px';
+  btn.style.padding = '0px';
+  const imgUrl = chrome.runtime.getURL('/assets/btn.png');
+  btn.style.backgroundImage = `url(${imgUrl})`;
+  btn.style.backgroundRepeat = 'no-repeat';
+  btn.style.backgroundSize = 'contain';
+  btn.style.cursor = 'pointer';
+  return btn;
+};
 
-btn.addEventListener('click', () => {
-  alert('Hello from Gmail!');
-});
+const getKeywords = () => {};
+
+const btn = createBtn();
 document.body.appendChild(btn);
+btn.addEventListener('click', () => {
+  const attachedFileName = document.querySelector('.vI')?.textContent;
+  console.log(attachedFileName);
+});
