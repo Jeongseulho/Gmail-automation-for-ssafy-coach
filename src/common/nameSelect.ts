560604761ab969ce0c64c4e22eecb6fb7a8b72e8
Jeong Seulho <wjdtmfgh@gmail.com>
2024-02-08 12:59:29 +0900

    feat : select name when double name file

---
+++ src/common/nameSelect.ts
@@ -0,0 +1,53 @@
+function createDialog(name1: string, name2: string, callback: (selectedName: string) => void) {
+  // dialog 요소 생성
+  const dialog = document.createElement('dialog');
+  dialog.setAttribute('style', 'border: solid black 1px; border-radius: 8px; padding: 0px; ');
+
+  // 컨테이너
+  const container = document.createElement('div');
+  container.setAttribute('style', 'padding: 10px;');
+
+  // 버튼 컨테이너
+  const buttonContainer = document.createElement('div');
+  buttonContainer.setAttribute('style', 'display: flex; gap: 10px; justify-content: center; align-items: center;');
+
+  // 제목 생성
+  const title = document.createElement('h3');
+  title.textContent = '작성자 이름을 선택해주세요.';
+
+  // name1 버튼 생성
+  const name1Button = document.createElement('button');
+  name1Button.textContent = name1;
+  name1Button.onclick = () => {
+    callback(name1);
+    dialog.close();
+    document.body.removeChild(dialog);
+  };
+
+  // name2 버튼 생성
+  const name2Button = document.createElement('button');
+  name2Button.textContent = name2;
+  name2Button.onclick = () => {
+    callback(name2);
+    dialog.close();
+    document.body.removeChild(dialog);
+  };
+
+  buttonContainer.appendChild(name1Button);
+  buttonContainer.appendChild(name2Button);
+
+  container.appendChild(title);
+  container.appendChild(buttonContainer);
+
+  dialog.appendChild(container);
+
+  document.body.appendChild(dialog);
+
+  dialog.showModal();
+}
+
+export const nameSelect = (name1: string, name2: string): Promise<string> => {
+  return new Promise((resolve) => {
+    createDialog(name1, name2, (selectedName: string) => resolve(selectedName));
+  });
+};
