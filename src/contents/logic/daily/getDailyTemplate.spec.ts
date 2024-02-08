8f97be87a05e57e8940f9573fe2a0ab2d202055d
Jeong Seulho <wjdtmfgh@gmail.com>
2024-02-08 20:48:50 +0900

    refactor : dailyTemplate test refactor, integrate

--- src/contents/logic/daily/getDailyTemplate.spec.ts
+++ src/contents/logic/daily/getDailyTemplate.spec.ts
@@ -1,12 +1,9 @@
 import { getDailyTemplate } from './getDailyTemplate';
 
 describe('getDailyTemplate 함수', () => {
-  const { title, content } = getDailyTemplate('240202', '구미', '1반', '이희진');
-  it('제목 템플릿을 반환한다', () => {
+  it('일일 보고서 템플릿을 반환한다', () => {
+    const { title, content } = getDailyTemplate('240202', '구미', '1반', '이희진');
     expect(title).toMatchSnapshot();
-  });
-
-  it('내용 템플릿을 반환한다', () => {
     expect(content).toMatchSnapshot();
   });
 });
