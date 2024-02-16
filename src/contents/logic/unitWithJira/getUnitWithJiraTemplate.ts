872344517bb387a0ee03b27e3179de7f4cd24f92
Jeong Seulho <wjdtmfgh@gmail.com>
2024-02-16 19:05:19 +0900

    fix : jira and unit template add formatdate

--- src/contents/logic/unitWithJira/getUnitWithJiraTemplate.ts
+++ src/contents/logic/unitWithJira/getUnitWithJiraTemplate.ts
@@ -1,10 +1,11 @@
 import { parseJira } from '@/contents/logic/jira/parseJira';
 import { nameSelect } from '@/contents/logic/common/nameSelect';
+import { formatDate } from '@/utils/formatDate';
 
 export const getUnitWithJiraTemplate = async (jiraFileName: string) => {
   const { date, project, campus, classGroup, week, name1, name2 } = parseJira(jiraFileName);
   const selectedName = await nameSelect(name1, name2);
-  const title = `[SSAFY] ${date} ${campus} ${classGroup} ${project} ${week} 주간 Jira 현황 보고서 및 단위 업무 보고서 송부`;
+  const title = `[SSAFY] ${formatDate(date)} ${campus} ${classGroup} ${project} ${week} 주간 Jira 현황 보고서 및 단위 업무 보고서 송부`;
   const content = `
     <div>안녕하세요.</div>
     <div>${campus} 실습코치 ${selectedName}입니다.</div>
