86a090ce09144a1fc0e34d0142447b2446b2f8fc
Jeong seulho <wjdtmfgh@gmail.com>
2024-03-12 14:32:25 +0900

    comment : add TODO, fileName validation

--- src/contents/logic/getFileCategory.ts
+++ src/contents/logic/getFileCategory.ts
@@ -1,6 +1,7 @@
 import { FILE_CATEGORY } from '@/constants/FileCategory';
 
 export const getFileCategory = (attachedFileName: string) => {
+  // TODO: 정규표현식으로 파일명 검사 및 분류 고도화
   for (const category of Object.values(FILE_CATEGORY)) {
     if (attachedFileName.includes(category)) {
       return category;
